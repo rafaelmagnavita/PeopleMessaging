eed37058a75cec9d6b4859352b1ce2947adf173e
= <=>
2023-11-01 20:10:59 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoRabbit/Program.cs
@@ -0,0 +1,20 @@
+﻿using RabbitMQ.Client;
+using RabbitProjectFiles.Models;
+using System;
+using System.Text.Json;
+
+namespace ProjetoRabbit
+{
+    class Program
+    {
+        public static ConnectionFactory conn = new ConnectionFactory { HostName = "localhost" };
+        public static Person rafa = new Person("Rafael", "1412826306", new DateTime(1997, 04, 29));
+        static void Main(string[] args)
+        {
+            var connection = conn.CreateConnection("curso-rabbitMQ");
+            var channel = connection.CreateModel();
+            var json = JsonSerializer.Serialize(rafa);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ RabbitProjectFiles/Models.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RabbitProjectFiles.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Document { get; set; }
+        public DateTime BirthDate { get; set; }
+        /// <summary>
+        /// Create new person
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="document"></param>
+        /// <param name="birthDate"></param>
+        public Person(string name, string document, DateTime birthDate)
+        {
+            Name = name;
+            Document = document;
+            BirthDate = birthDate;
+        }
+    }
+}
