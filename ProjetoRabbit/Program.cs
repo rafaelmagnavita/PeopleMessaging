7e2caf6250abdb4d87a84ba28f25d581dcdebc03
= <=>
2023-11-07 14:48:04 -0300

    enviando requisições

--- ProjetoRabbit/Program.cs
+++ ProjetoRabbit/Program.cs
@@ -1,6 +1,7 @@
 ﻿using RabbitMQ.Client;
 using RabbitProjectFiles.Models;
 using System;
+using System.Text;
 using System.Text.Json;
 
 namespace ProjetoRabbit
@@ -9,12 +10,15 @@ namespace ProjetoRabbit
     {
         public static ConnectionFactory conn = new ConnectionFactory { HostName = "localhost" };
         public static Person rafa = new Person("Rafael", "1412826306", new DateTime(1997, 04, 29));
+        const string EXCHANGE = "curso-rabbitmq";
         static void Main(string[] args)
         {
             var connection = conn.CreateConnection("curso-rabbitMQ");
             var channel = connection.CreateModel();
             var json = JsonSerializer.Serialize(rafa);
-            Console.WriteLine("Hello World!");
+            var byteArray = Encoding.UTF8.GetBytes(json);
+            channel.BasicPublish(EXCHANGE, "rh.person-created", null, byteArray);
+            Console.WriteLine($"Message Published: {json}");
         }
     }
 }
